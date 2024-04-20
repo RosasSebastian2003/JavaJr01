92b66c1bda85b05cae2c59ec270a690c7b3dc0af
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2024-04-20 11:35:53 -0600

    La clase pantalla esta terminada

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Pantalla samsungSmartTv = new Pantalla();
+        
+        samsungSmartTv.setMarca(ScreenBrands.Samsung);
+        
+        Pantalla samsungSmartLedTv = new Pantalla("Negro", "Lcd SmartTV 24 in", "Samsung", "LED", 24, (float) 1123.80);
     }
 }
--- Pantalla.java
+++ Pantalla.java
@@ -1,8 +1,28 @@
 public class Pantalla {
-    String color, modelo, marca, tipo;
-    int resolucion;
+    String color;
+    String tipo; 
     float precio;
     boolean isOn;
+
+    private String modelo;
+    private String marca;
+    private int resolucion;
+
+    Pantalla() {
+
+    }
+
+    // Constructor parametrado
+    Pantalla(String color, String modelo, String marca, String tipo, int resolucion, float precio) {
+        this.color = color;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.tipo = tipo;
+        this.resolucion = resolucion;
+        this.precio = precio;
+
+        isOn = false;
+    }
 
     void isOn() {
         if (isOn) {
@@ -12,7 +32,7 @@ public class Pantalla {
         }
     }
 
-    boolean toggle() {
+    private boolean toggle() {
         if (isOn) {
             isOn = false;
             return isOn;
@@ -20,5 +40,37 @@ public class Pantalla {
             isOn = true;
             return isOn;
         }
+    }
+
+    // Getters
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getResolucion() {
+        return resolucion;
+    }
+
+    // Setters
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    // String como parametro
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    // Enumerador como atributo
+    public void setMarca(ScreenBrands marca) {
+        this.marca = marca.toString();
+    }
+
+    public void setResolucion(int resolucion) {
+        this.resolucion = resolucion;
     }
 }
